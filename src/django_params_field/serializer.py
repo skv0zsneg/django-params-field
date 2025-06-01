ae1f88e8993325d1b2ecab6ae3206937e211fd05
skv0zsneg <klimentlamonov@yandex.ru>
2025-06-01 16:38:19 +0300

    Add more tests & minor fix.

--- src/django_params_field/serializer.py
+++ src/django_params_field/serializer.py
@@ -1,20 +1,27 @@
 import bz2
 import pickle
-from typing import Any
+from typing import Any, ClassVar, Generic, TypeVar
+
+P = TypeVar("P")
 
 
-class Serializer:
+class Serializer(Generic[P]):
     """Params serializer & deserializer."""
 
-    def __init__(self, params_type: Any | None = None) -> None:
+    ALLOWED_TYPES: ClassVar[tuple[Any, ...]] = (dict,)
+
+    def __init__(self, params_type: P | None = None) -> None:
         self.params_type = params_type
 
-    def serialize(self, value: Any) -> bytes:
+    def serialize(self, value: P) -> bytes:
         serialized = pickle.dumps(value)
         compressed = bz2.compress(serialized)
         return compressed
 
-    def deserialize(self, value: bytes) -> Any:
+    def deserialize(self, value: bytes) -> P:
         decompressed = bz2.decompress(value)
         deserialized = pickle.loads(decompressed)
         return deserialized
+
+    def is_valid(self, value: P) -> bool:
+        return any(map(lambda allowed_type: isinstance(value, allowed_type), self.ALLOWED_TYPES))

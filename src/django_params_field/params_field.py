9ed388449399b09549e92163837a6a0934e5c2ce
skv0zsneg <klimentlamonov@yandex.ru>
2025-06-09 00:05:17 +0300

    Fix typing for ParamsField & Serializer. Plus minor fix.

--- src/django_params_field/params_field.py
+++ src/django_params_field/params_field.py
@@ -3,31 +3,31 @@ from typing import Any, Generic, Optional
 from django.core.exceptions import ValidationError
 from django.db.models.fields import BinaryField
 
-from django_params_field.serializer import P, Serializer
+from django_params_field.serializer import V, Serializer
 
 
-class ParamsField(BinaryField, Generic[P]):
+class ParamsField(BinaryField, Generic[V]):
     """Storing a set of params in one field."""
 
-    def __init__(self, params_type: Optional[P] = None, *args, **kwargs) -> None:
-        self.params_type = params_type
-        self.serializer = Serializer(self.params_type)
+    def __init__(self, schema: Optional[V] = None, *args, **kwargs) -> None:
+        self.schema = schema
+        self.serializer = Serializer(self.schema)
         super().__init__(*args, **kwargs)
 
     def deconstruct(self) -> tuple[Any, Any, Any, Any]:
         name, path, args, kwargs = super().deconstruct()
-        kwargs["params_type"] = self.params_type
+        kwargs["schema"] = self.schema
         return name, path, args, kwargs
 
-    def from_db_value(self, value: bytes, *args, **kwargs) -> P:
+    def from_db_value(self, value: bytes, *args, **kwargs) -> V:
         return self.serializer.deserialize(value)
 
-    def to_python(self, value: P) -> P:
+    def to_python(self, value: V) -> V:
         return value
 
-    def get_db_prep_value(self, value: P, *args, **kwargs) -> bytes:
+    def get_db_prep_value(self, value: V, *args, **kwargs) -> bytes:
         return self.serializer.serialize(value)
 
-    def validate(self, value: P, *args, **kwargs) -> None:
+    def validate(self, value: V, *args, **kwargs) -> None:
         if not self.serializer.is_valid(value):
             raise ValidationError(f"Given value '{value}' is not valid!")
--- src/django_params_field/serializer.py
+++ src/django_params_field/serializer.py
@@ -2,30 +2,32 @@ import bz2
 import pickle
 from typing import Any, ClassVar, Generic, Optional, TypeVar
 
-P = TypeVar("P")
+from django_params_field.schema_validator import S, SchemaValidator
+
+V = TypeVar("V")
 
 
-class Serializer(Generic[P]):
+class Serializer(Generic[V]):
     """Params serializer & deserializer."""
 
-    ALLOWED_TYPES: ClassVar[tuple[Any, ...]] = (dict,)
+    ALLOWED_VALUE_TYPES: ClassVar[tuple[Any, ...]] = (dict,)
 
-    def __init__(self, params_type: Optional[P] = None) -> None:
-        self.params_type = params_type
+    def __init__(self, schema: Optional[S] = None) -> None:
+        self.schema = SchemaValidator(schema)
 
-    def serialize(self, value: P) -> bytes:
+    def serialize(self, value: V) -> bytes:
         serialized = pickle.dumps(value)
         compressed = bz2.compress(serialized)
         return compressed
 
-    def deserialize(self, value: bytes) -> P:
+    def deserialize(self, value: bytes) -> V:
         decompressed = bz2.decompress(value)
         deserialized = pickle.loads(decompressed)
         return deserialized
 
-    def is_valid(self, value: P) -> bool:
+    def is_valid(self, value: V) -> bool:
         return any(
             map(
-                lambda allowed_type: isinstance(value, allowed_type), self.ALLOWED_TYPES
+                lambda allowed_type: isinstance(value, allowed_type), self.ALLOWED_VALUE_TYPES
             )
         )
